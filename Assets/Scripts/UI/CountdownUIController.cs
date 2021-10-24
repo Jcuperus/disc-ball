4882d1fe8f310bfb928e9aed1f1d108d3bddba9a
Jaep Cuperus <jaepcuperus@hotmail.com>
2021-10-24 19:06:51 +0200

    Counter label animation

--- Assets/Scripts/UI/CountdownUIController.cs
+++ Assets/Scripts/UI/CountdownUIController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Helpers;
 using TMPro;
 using UnityEngine;
@@ -7,9 +8,14 @@ namespace UI
     public class CountdownUIController : MonoBehaviour
     {
         [SerializeField] private TMP_Text countdownLabel;
+        [SerializeField] private Animator labelAnimator;
 
         private GameObject panel;
-        
+
+        private static readonly int FadeInTrigger = Animator.StringToHash("FadeIn");
+        private static readonly int FadeOutTrigger = Animator.StringToHash("FadeOut");
+
+
         private void Awake()
         {
             panel = transform.GetChild(0).gameObject;
@@ -22,8 +28,18 @@ namespace UI
         {
             if (count <= 0) panel.SetActive(false);
             else if (!panel.activeSelf) panel.SetActive(true);
+
+            StartCoroutine(LabelFadeCoroutine(count));
+        }
+
+        private IEnumerator LabelFadeCoroutine(int newCount)
+        {
+            labelAnimator.SetTrigger(FadeOutTrigger);
+
+            yield return new WaitForSeconds(labelAnimator.GetCurrentAnimatorStateInfo(0).length);
             
-            countdownLabel.text = count.ToString();
+            countdownLabel.text = newCount.ToString();
+            labelAnimator.SetTrigger(FadeInTrigger);
         }
     }
 }
