43b3b2c5e80122722d5cf0e140e35995e67a6b7c
Jaep Cuperus <jaepcuperus@hotmail.com>
2021-10-26 17:49:25 +0200

    Updated lighting setup

---
+++ Assets/Scripts/LookAtObject.cs
@@ -0,0 +1,12 @@
+using Helpers;
+using UnityEngine;
+
+public class LookAtObject : MonoBehaviour
+{
+    [SerializeField] private GameObject subject;
+    
+    private void Update()
+    {
+        transform.LookAt(subject.transform.position);
+    }
+}
