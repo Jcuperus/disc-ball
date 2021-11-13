35e449afa6d29c18aa0ca9e78d0fcfadeb9a9fe0
Jaep Cuperus <jaepcuperus@hotmail.com>
2021-11-13 16:29:40 +0100

    Minor UI refactor

--- Assets/Scripts/UI/MainMenuUIController.cs
+++ Assets/Scripts/UI/MainMenuUIController.cs
@@ -1,28 +1,41 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class MainMenuUIController : MonoBehaviour
     {
+        [SerializeField] private Button startButton, customButton, settingsButton, quitButton;
         [SerializeField] private TMP_Text startButtonLabel;
         [SerializeField] private GameObject customGamePanel;
 
         private const string LoadingText = "Loading...";
-        
-        public void OnStartClicked()
+
+        private void Awake()
+        {
+            startButton.onClick.AddListener(OnStartClicked);
+            customButton.onClick.AddListener(OnCustomGameClicked);
+            settingsButton.onClick.AddListener(OnSettingsClicked);
+            quitButton.onClick.AddListener(Application.Quit);
+        }
+
+        private void OnStartClicked()
         {
             GameConfigurationManager.Instance.GameConfig = GameConfigurationManager.Instance.DefaultConfig;
             MenuManager.StartGame();
             startButtonLabel.text = LoadingText;
         }
 
-        public void OnCustomGameClicked()
+        private void OnSettingsClicked()
+        {
+            
+        }
+
+        private void OnCustomGameClicked()
         {
             customGamePanel.SetActive(!customGamePanel.activeSelf);
         }
-
-        public void OnQuitClicked() => Application.Quit();
     }
 }
 
