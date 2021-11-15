3c65cd71fd31c007e45c616c1416d3afa2e2efdc
Jaep Cuperus <jaepcuperus@hotmail.com>
2021-11-15 21:45:07 +0100

    Changed spotlight follow

---
+++ Assets/Scripts/Gameplay/FollowObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Gameplay
+{
+    public class FollowObject : MonoBehaviour
+    {
+        [SerializeField] private Transform subjectTransform;
+
+        private Vector3 initialOffset;
+        private readonly Vector3 vectorXY = new Vector3(1f, 1f, 0);
+        
+        private void Awake()
+        {
+            initialOffset = Vector3.Scale(transform.position - subjectTransform.position, vectorXY);
+        }
+
+        private void Update()
+        {
+            Vector3 subjectPosition = subjectTransform.position;
+            transform.position = Vector3.Scale(subjectPosition, vectorXY) + initialOffset;
+            
+            Vector3 lookDirection = subjectPosition - transform.position;
+            transform.rotation = Quaternion.LookRotation(lookDirection);
+        }
+    }
+}
